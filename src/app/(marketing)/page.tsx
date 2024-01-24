3e571a73eeeba64e41b9bc145e0e41c8c77a0938
rcsen <rohansen856@gmail.com>
2024-01-25 02:35:57 +0530

    modified:   src/app/(marketing)/page.tsx 	modified:   src/components/site-footer.tsx

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -27,23 +27,7 @@ export default async function DashboardPage() {
 
     return (
         <>
-            <div className="md:hidden">
-                <Image
-                    src="/examples/dashboard-light.png"
-                    width={1280}
-                    height={866}
-                    alt="Dashboard"
-                    className="block dark:hidden"
-                />
-                <Image
-                    src="/examples/dashboard-dark.png"
-                    width={1280}
-                    height={866}
-                    alt="Dashboard"
-                    className="hidden dark:block"
-                />
-            </div>
-            <div className="hidden flex-col md:flex">
+            <div className="flex flex-col md:flex">
                 <div className="flex-1 space-y-4 p-8 pt-6">
                     <div className="flex items-center justify-between space-y-2">
                         <h2 className="text-3xl font-bold tracking-tight">
